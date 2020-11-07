7cc8d069815f891a18c2931cf8ea85e47b2e1b13
Iliya Zhecev <iliya.zhechev@gmail.com>
2020-11-07 19:31:14 +0200

    Update loading of svg

--- assets/scripts/one-d-gan/utils.js
+++ assets/scripts/one-d-gan/utils.js
@@ -1,27 +1,28 @@
 // Works with SVG diagrams generated with <draw.io>
 function prepareDiagramElement({svgId, textContent}) {
-    const svg = document.getElementById(svgId).contentDocument
+    // const svg = document.getElementById(svgId).contentDocument;
+    const svg = document.getElementById(svgId);
 
     // This comes from the way draw.io rendered to svg
     const divs = Array.from(svg.querySelectorAll('div'))
         .filter(el => el.textContent === textContent);
     const contentSelected = divs[divs.length - 1];
-    const idSelected = svg.getElementById(textContent);
+    const idSelected = document.getElementById(textContent);
     const div = contentSelected || idSelected;
 
     const rectSibling = div.parentElement.parentElement.parentElement.parentElement.parentElement.previousElementSibling;
-
-    const width = rectSibling.attributes.width.value;
-    const height = rectSibling.attributes.height.value;
+    // const {width, height} = rectSibling.getBoundingClientRect();
+    const {width, height} = rectSibling.getBBox();
+    const [W, H] = [width, height].map(Math.round).map(a => a - 6);
 
     div.style.display = 'block';
-    div.style.width = `${width}px`;
-    div.style.height = `${height}px`;
+    div.style.width = `${W}px`;
+    div.style.height = `${H}px`;
     div.style.overflow = 'hidden';
     div.id = textContent;
     div.innerText = '';
 
-    return [div, {width, height}];
+    return [div, {width: W, height: H}];
 }
 
 function monkeyPatchSVGContext(svgId, handler) {
@@ -31,3 +32,14 @@ function monkeyPatchSVGContext(svgId, handler) {
     handler();
     window.HTMLElement = oldElementCtor;
 };
+
+// SRC - <https://stackoverflow.com/a/14070928>
+async function loadSVGs({selector, dataAttr='data-url'} = {}) {
+    const promises = Array.from(document.querySelectorAll(selector)).map(async e => {
+        const svgUrl = e.attributes[dataAttr].value;
+        const content = await fetch(svgUrl);
+        e.innerHTML = await content.text();
+    });
+
+    await Promise.all(promises);
+}
