d17b67201893e641ea242db4c8bcd0df723dc002
Iliya Zhechev <iliya.zhechev@hyperscience.com>
2021-07-12 12:37:44 +0300

    Use web worker in index js

--- assets/one-d-gan/scripts/index.js
+++ assets/one-d-gan/scripts/index.js
@@ -13,54 +13,62 @@ requirejs.config({
 requirejs(["tf", "gan", "ui", "utils"], () => (window.onload = () => main()));
 
 async function main() {
-  const tf = require("tf");
-  const { data, OneDGAN } = require("gan");
   const ui = require("ui");
-  const { loadSVGs, wait } = require("utils");
-
-  const generateData = data.bimodal(0.3);
+  let t = 0;
+  const { loadSVGs } = require("utils");
 
   await loadSVGs({
     selector: ".svg",
   });
+  var updateGANView, updateDBox, updateLoss, updateGANOutputs, updateDOutputBox;
+  let predicting = false;
 
-  const waitALittle = () => wait(10);
+  const worker = new Worker("/assets/one-d-gan/scripts/worker.js");
+
+  worker.onmessage = async ({ data }) => {
+    if (data.type == "init") {
+      const initData = data.payload;
+      await init(false, true);
+      resolveLoading("main-demo");
+
+      ui.initInputDataUI(initData.fixedInputSync);
+      updateGANView = ui.initGANViewUI(
+        initData.rangeInputSync,
+        initData.gPredictRangeInput
+      );
+
+      updateDBox = ui.initDBoxUI(
+        initData.rangeInputSync,
+        initData.dPredictRangeInput
+      );
+      updateLoss = ui.initLossUI();
+
+      updateGANOutputs = await ui.initGANOutputUI(
+        initData.fakeOutputsSync,
+        initData.targetDataSync
+      );
+      updateDOutputBox = await ui.initDOutputUI(
+        initData.dPredictTargetData,
+        initData.dPredictFakeData
+      );
+    }
+
+    if (data.type == "optim-step:response") {
+      const optimData = data.payload;
+
+      updateGANOutputs(optimData.fakeOutputsSync);
+      updateDOutputBox(optimData.dTargetDataSync, optimData.dFakeOutputsSync);
+
+      updateGANView(optimData.ganRangeOutputSync);
+      updateDBox(optimData.ganRangeDOutputSync);
+      updateLoss(optimData.dLoss, optimData.gLoss);
+
+      predicting = false;
+    }
+  };
 
   async function init(running, firstCall) {
-    let t = 0;
-    const inpDims = 1;
-    const gan = new OneDGAN(0.005, inpDims);
-
-    const targetData = generateData(512);
-    const targetDataSync = targetData.dataSync();
-    const fixedInputs = gan.sampleZ([512, inpDims]);
-    const fixedInputSync = fixedInputs.dataSync();
-    const fakeOutputs = gan.G.predict(fixedInputs);
-    const fakeOutputsSync = fakeOutputs.dataSync();
-
-    const rangeInputs = tf.range(-3, 3, 0.1).expandDims(1);
-    const rangeInputSync = rangeInputs.dataSync();
-
-    ui.initInputDataUI(fixedInputSync);
-    const updateGANView = ui.initGANViewUI(
-      rangeInputSync,
-      gan.G.predict(rangeInputs).dataSync()
-    );
-
-    const updateDBox = ui.initDBoxUI(
-      rangeInputSync,
-      gan.D.predict(rangeInputs).dataSync()
-    );
-    const updateLoss = ui.initLossUI();
-
-    const updateGANOutputs = await ui.initGANOutputUI(
-      fakeOutputsSync,
-      targetDataSync
-    );
-    const updateDOutputBox = await ui.initDOutputUI(
-      gan.D.predict(targetData).dataSync(),
-      gan.D.predict(fakeOutputs).dataSync()
-    );
+    t = 0;
 
     const playPauseEl = document.getElementById("play-pause");
     const itInfoEl = document.getElementById("iteration-info");
@@ -82,64 +90,23 @@ async function main() {
       running = false;
     };
 
-    // This is done for optimization (single pass of the generator)
-    const combinedGInput = fixedInputs.concat(rangeInputs);
-    const combinedDInput = rangeInputs.concat(fakeOutputs).concat(targetData);
-
-    async function step(i) {
-      const bs = i == 0 ? 2 : 128;
-      const batch = generateData(bs);
-      const loss = await gan.optim_step(batch);
-      const [gLoss, dLoss] = loss;
-
-      console.log(`[${i}] Loss: ${loss}`);
-
-      const combinedGOutput = gan.G.predict(combinedGInput);
-      const [fakeOutputsSync, ganRangeOutputSync] = [
-        combinedGOutput.slice([0], [fixedInputs.shape[0]]).dataSync(),
-        combinedGOutput
-          .slice([fixedInputs.shape[0]], [rangeInputs.shape[0]])
-          .dataSync(),
-      ];
-
-      const combinedDOutput = gan.D.predict(combinedDInput);
-      const [ganRangeDOutputSync, dFakeOutputsSync, dTargetDataSync] = [
-        combinedDOutput.slice([0], [rangeInputs.shape[0]]).dataSync(),
-        combinedDOutput
-          .slice([rangeInputs.shape[0]], [fakeOutputs.shape[0]])
-          .dataSync(),
-        combinedDOutput
-          .slice([fakeOutputs.shape[0]], [targetData.shape[0]])
-          .dataSync(),
-      ];
-
-      if (i % 3 == 0) {
-        updateGANOutputs(fakeOutputsSync);
-        updateDOutputBox(dTargetDataSync, dFakeOutputsSync);
+    async function step() {
+      if (!predicting) {
+        predicting = true;
+        worker.postMessage({ type: "optim-step:request" });
+        itInfoEl.innerText = "#" + t.toString().padStart(4, 0);
       }
-
-      updateGANView(ganRangeOutputSync);
-      updateDBox(ganRangeDOutputSync);
-      updateLoss(dLoss, gLoss);
     }
 
     async function loop() {
       if (running) {
         t += 1;
-        await step(t);
-        itInfoEl.innerText = "#" + t.toString().padStart(4, 0);
+        await step();
       }
 
       window.requestAnimationFrame(loop);
     }
 
     loop();
-
-    if (firstCall) {
-      await step(0);
-    }
   }
-
-  await init(false, true);
-  resolveLoading("main-demo");
 }
